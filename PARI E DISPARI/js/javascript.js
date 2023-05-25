c72cf8de0e69e47fd16a3cfc65a401655a179c24
Anieb22 <anielloverdino@gmail.com>
2023-05-25 20:26:30 +0200

    aggiunto javascript esercizio concluso

---
+++ PARI E DISPARI/js/javascript.js
@@ -0,0 +1,42 @@
+let bottonePari = document.getElementById('bottone-pari')
+let bottoneDispari = document.getElementById('bottone-dispari')
+let somma = 0;
+
+
+
+function sommaNumeri(){
+    let sceltaUtente = parseInt(prompt('scegli un numero da 1 a 5'))
+    let sceltasCpu = Math.floor(Math.random() * 5)
+    somma = parseInt(sceltaUtente + sceltasCpu)
+    return somma;
+}
+
+bottonePari.addEventListener('click', function(){
+   sommaNumeri()
+
+    if (somma % 2 === 0){
+        console.log(somma)
+        console.log('Il giocatore vince')
+    }
+
+    else{
+        console.log(somma)
+        console.log('Il giocatore perde')
+    }
+})
+
+bottoneDispari.addEventListener('click', function(){
+    ('prova b-dispari');
+    sommaNumeri()
+
+    if (somma % 2 === 0){
+        console.log(somma)
+        console.log('Il giocatore perde')
+    }
+
+    else{
+        console.log(somma)
+        console.log('Il giocatore vince')
+    }
+
+})
